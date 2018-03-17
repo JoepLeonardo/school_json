33ad714d019c85adb01d210e069510208cb866af
rpi <jlbenten@avans.nl>
2018-03-17 20:24:10 +0100

    *guiField.py extended

--- guiField.py
+++ guiField.py
@@ -15,30 +15,46 @@ class GuiField(DisplayOnMonitor):
     # colors
     colorWhite = (255, 255, 255)
     colorBlack = (0, 0, 0)
+    colorDebug = (155, 255, 0)
+    
+    # border
+    border_width = 5
+    border_height = 20
+       
            
-    def clearSurface(self):
-        print(self)
+    def fieldClear(self):
         # make the surface black
         self.surfaceScreen.fill(self.colorBlack)
         
         # draw board border
-        pygame.draw.rect(self.surfaceScreen, self.colorWhite, pygame.Rect(0, self.surfaceTop, self.surfaceWidth, self.surfaceHeight-self.surfaceTop), 5)
+        pygame.draw.rect(self.surfaceScreen, self.colorWhite, pygame.Rect(0, self.surfaceTop, self.surfaceWidth, self.surfaceHeight-self.surfaceTop), self.border_width)
         # draw board half line
         half_line_x = self.surfaceWidth/2
         half_line_y = self.surfaceTop
-        half_line_w = 5
-        half_line_h = 20
         while half_line_y < self.surfaceHeight:
-            pygame.draw.rect(self.surfaceScreen, self.colorWhite, pygame.Rect(half_line_x, half_line_y, half_line_w, half_line_h))
-            half_line_y += (half_line_h*2)
-            
+            pygame.draw.rect(self.surfaceScreen, self.colorWhite, pygame.Rect(half_line_x, half_line_y, self.border_width, self.border_height))
+            half_line_y += (self.border_height*2)
+                       
+    def fieldAddPlayer(self, posX, posY, sizeX, sizeY, points):
+        # draw players bad
+        pygame.draw.rect(self.surfaceScreen, self.colorWhite, pygame.Rect((posX), (posY), sizeX, sizeY))
+        # TODO: draw players score
+        
+    def fieldDisplay(self):
         # display the surface
-        self.display(self.surfaceScreen) 
+        self.display(self.surfaceScreen)
     
+    def getFieldWidth(self):
+        # Width of the black part of the field
+        fieldWidth = self.surfaceWidth -(self.border_width*2)
+        return fieldWidth
 
-    def getSurfaceWidth():
-        return surfaceWidth
+    def getFieldHeight(self):
+        # Height of the black part of the field
+        fieldHeight = self.surfaceHeight -self.surfaceTop -(self.border_width*2)
+        return fieldHeight
+    
+    def getBorderWidth(self):
+        return self.border_width
 
-    def getSurfaceHeight():
-        return surfaceHeight
     
