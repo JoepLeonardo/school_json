62027da0fd247969924f49881a7ea930ceb34f9d
rpi <jlbenten@avans.nl>
2018-04-05 11:36:52 +0200

    Added a inputhandler class but it cant be used before the controllers and console board are ready

---
+++ inputHandler.py
@@ -0,0 +1,38 @@
+import pygame
+from pygame.locals import *
+import sys
+
+class InputHandler():
+    
+    def getConsole(self):
+        data = 0
+        for event in pygame.event.get():
+            # Check if 'esc' or close button is pressed
+            if (event.type == QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)):
+                data = 1
+        return data
+                
+    def getController1(self):
+        data = 0
+        for event in pygame.event.get():
+            # Check if 'w' button is pressed
+            if (event.type == pygame.KEYDOWN and event.key == pygame.K_w):
+                data = -1
+            # Check if 's' button is pressed
+            elif (event.type == pygame.KEYDOWN and event.key == pygame.K_s):
+                data = 1
+        return data
+                
+    def getController2(self):
+        data = 0
+        for event in pygame.event.get():
+            # Check if 'up' button is pressed
+            if (event.type == pygame.KEYDOWN and event.key == pygame.K_UP):
+                data = -1
+            # Check if 'down' button is pressed
+            elif (event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN):
+                data = 1
+        return data
+            
+            
+    
--- pongGame.py
+++ pongGame.py
@@ -1,3 +1,4 @@
+from inputHandler import InputHandler
 from guiField import GuiField
 from player import Player
 from ball import Ball
@@ -12,9 +13,9 @@ clock = pygame.time.Clock()
 
 class PongGame():
 
-###########################
-###### GAME SETTINGS ######
-###########################
+#############################
+######  GAME SETTINGS  ######
+#############################
 
     # Set player bad size
     playerSizeX = 15
@@ -24,26 +25,12 @@ class PongGame():
     ballSize = 15
     ballSpeed = 15
 
-#############################
-###### END OF SETTINGS ######
-#############################
-
-    # variables
-    playPong=True
-    gameState = None
-    statePlayNormal = 0
-    statePlayer1Scored = 1
-    statePlayer2Scored = 2
-    dirPlayerMissedBall = 9
-    dirUp = -1
-    dirDown = 1
-    dirLeft = -1
-    dirRight = 1 
-    dirNormal = 0
-
-    debugMainLoopCnt = 0
+###############################
+######  END OF SETTINGS  ######
+###############################
 
     # make objects
+    input = InputHandler()
     guiField = GuiField()
     player1 = Player(guiField.getFieldStartX(), playerSizeX, playerSizeY, guiField.getFieldStartY(), (guiField.getFieldEndY()-playerSizeY))
     player2 = Player((guiField.getFieldEndX()-playerSizeX), playerSizeX, playerSizeY, guiField.getFieldStartY(), (guiField.getFieldEndY()-playerSizeY))
@@ -54,6 +41,20 @@ class PongGame():
     player2WallX = guiField.getFieldEndX() - playerSizeX
     fieldHeigtMiddle = (guiField.getFieldHeight()/2)
     fieldWidthMiddle = (guiField.getFieldWidth()/2)
+    # variables
+    playPong=True
+    gameState = None
+    statePlayNormal = 0
+    statePlayer1Scored = 1
+    statePlayer2Scored = 2
+    dirUp = -1
+    dirDown = 1
+    dirLeft = -1
+    dirRight = 1 
+    dirNormal = 0
+    dirPlayerMissedBall = 9
+    # debug variables
+    debugMainLoopCnt = 0
 
     def resetGame(self):
         # Reset the ball
@@ -84,39 +85,59 @@ class PongGame():
         # return the direction of the ball
         return dir    
 
-    def update_ball(self):
+    def updateGame(self):
         for loopCnt in range(0, self.ball.getSpeed()):
             #print("for update")
-            dirX = self.ball.getDirX()
-            dirY_OrGameOver = self.ball.getDirY()
+            ballDirX = self.ball.getDirX()
+            ballDirY_OrGameOver = self.ball.getDirY()
             
             #Check ball hits wall player1
             if((self.ball.getPosX()+self.ball.getDirX()) == self.player1WallX):
-                dirX = self.dirRight
-                dirY_OrGameOver = self.ballHitPlayerDirection(self.player1.getPosY(), self.ball.getPosY())
+                ballDirX = self.dirRight
+                ballDirY_OrGameOver = self.ballHitPlayerDirection(self.player1.getPosY(), self.ball.getPosY())
             #Check ball hits wall player2
             elif((self.ball.getPosX()+self.ball.getDirX()+self.ballSize) == self.player2WallX):
-                dirX = self.dirLeft
-                dirY_OrGameOver = self.ballHitPlayerDirection(self.player2.getPosY(), self.ball.getPosY())
+                ballDirX = self.dirLeft
+                ballDirY_OrGameOver = self.ballHitPlayerDirection(self.player2.getPosY(), self.ball.getPosY())
             #Check ball hits top
             elif((self.ball.getPosY()+self.ball.getDirY()) == self.guiField.getFieldStartY()):
-                dirY_OrGameOver = self.dirDown
+                ballDirY_OrGameOver = self.dirDown
             #Check ball hits bottom
             elif((self.ball.getPosY()+self.ball.getDirY()+self.ballSize) == self.guiField.getFieldEndY()):
-                dirY_OrGameOver = self.dirUp
+                ballDirY_OrGameOver = self.dirUp
             
-            if(dirY_OrGameOver != self.dirPlayerMissedBall):
+            if(ballDirY_OrGameOver != self.dirPlayerMissedBall):
                 # update ball dir and position
-                self.ball.updateDir(dirX, dirY_OrGameOver)
+                self.ball.updateDir(ballDirX, ballDirY_OrGameOver)
                 self.ball.updatePos()
             else:
-                #check who won via dirX left or right
-                self.gameState = self.statePlayer1Scored
-                # end for loop
+                #check who won via ballDirX left or right
+                if (ballDirX == self.dirRight):
+                    self.gameState = self.statePlayer2Scored
+                else:
+                    self.gameState = self.statePlayer1Scored
+                # end the loop
                 loopCnt = self.ballSpeed
-                
                     
-    def handle_input(self):
+    def handleConsoleinput(self):
+        data = self.input.getConsole()
+        if (data == 1):
+            self.playPong = False
+        
+    def handleControllerInput(self):
+        pixelsToMove = 20
+        # update player1 pos
+        data = self.input.getController1()
+        if (data != 0):
+            print("data1 " + str(data))
+        self.player1.move((data*pixelsToMove));
+        # update player2 pos
+        data = self.input.getController2()
+        if (data != 0):
+            print("data2 " + str(data))
+        self.player2.move((data*pixelsToMove));
+        
+    def handleInput(self):
         pixelsToMove = 20
         for event in pygame.event.get():
             # Check if 'esc' or close button is pressed
@@ -138,7 +159,6 @@ class PongGame():
             elif (event.type == pygame.KEYDOWN and event.key == pygame.K_s):
                 # move player 2 100 pixels down
                 self.player1.move(pixelsToMove); 
-        return True
 
     def displayGame(self):
         # draw players and ball
@@ -160,18 +180,20 @@ class PongGame():
             # debug timer info
             #print(str(debugMainLoopCnt) + " tick0 " + str(pygame.time.get_ticks()))
             #debugMainLoopCnt = debugMainLoopCnt+1
+                        
+            #self.handleControllerInput()  Can only be implemented when controllers are done
+            #self.handleConsoleinput()     Can only be implemented when console board is done
+            self.handleInput()
             
             if (self.gameState == self.statePlayNormal):
                 self.displayGame()
-                self.update_ball()
+                self.updateGame()
             elif (self.gameState == self.statePlayer1Scored):
                 self.player1.addPoint()
                 self.resetGame()
             elif (self.gameState == self.statePlayer2Scored):
-                player2.addPoint()
-                self.resetGame()
-                    
-            self.handle_input()
+                self.player2.addPoint()
+                self.resetGame()                          
             
             # debug timer info
             #print(str(debugMainLoopCnt) + " tick1 " + str(pygame.time.get_ticks()-1))
